b6e1ad450316717f0cf98141cd997a82b8463cbe
Fleur Van De Ville <fleur.van.de.ville@student.ehb.be>
2023-03-22 16:10:52 +0100

    oefening bezig

---
+++ js/Joke.js
@@ -0,0 +1,23 @@
+"use strict";
+
+export default class Joke {
+    constructor(setup, delivery){
+        this.setup = setup;
+        this.delivery = delivery;
+    }
+
+    htmlString(){
+        `<div id="list">
+        <div class="item">
+          <div>
+            <div class="item-field">${this.setup}</div>
+            <div class="item-field">${this.delivery}</div>
+          </div>
+          
+          <div>
+            <img src="https://opendata.brussels.be/explore/dataset/street-art/files/9876c9a3f300f29c8ee619765c1ad768/300/" />
+          </div>
+
+        </div>`;
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,15 +1,31 @@
 "use strict";
 
+import Joke from "./Joke.js";
+
 const app = {
     items: [],
     init() {
+        this.getData();
     },
     getData() {
+        fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,racist,sexist,explicit&amount=10&type=twopart')
+        .then(function (response){
+            return response.json();
+        })
+        .then(function(json){
+            json.jokes.forEach(function(jokeData){
+                const joke = new Joke(jokeData.setup, jokeData.delivery);
+                console.log(Joke);
+            });
+
+            app.render();
+        });
     },
     onSearch() {
     },
     render(message) {
+        //document.querySelector('#list').insertAdjacentHTML;
     }
 
-}
-app.init()
+};
+app.init();
